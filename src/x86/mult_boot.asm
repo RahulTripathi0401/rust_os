8e8f3a065e670d72237bde40776d30dbeaaaa766
Rahul Tripathi <rahultripathi0401@gmail.com>
2021-12-27 13:16:58 +1100

    boot

--- src/x86/mult_boot.asm
+++ src/x86/mult_boot.asm
@@ -1,11 +1,11 @@
-section .multiboot_header ; section named mutli_boot_header 
+section .multiboot_header 
 header_start:
-    dd 0xe85250d6                ; magic number (multiboot 2) dd stands for define double
-                                 ; 32 bit value
-    dd 0                         ; architecture 0 (protected mode i386)
-    dd header_end - header_start ; header length
+    dd 0xe85250d6                
+                                 
+    dd 0 ; 0 for i386 or 4 for MIPS                         
+    dd header_end - header_start 
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-    dw 0    ; type
-    dw 0    ; flags
-    dd 8    ; size
+    dw 0    
+    dw 0    
+    dd 8    
 header_end:
